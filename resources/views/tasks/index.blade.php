a6cfa9f2df20800864d3cde659009fd23cdd97c3
三戸尚樹 <>
2018-10-28 16:55:21 +0900

    css変更

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,38 +1,32 @@
-<!DOCTYPE html>
-<html>
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ env('APP_NAME') }}</title>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-  <div class="container align-middle mt-5">
-    <h1 class="display-6 d-inline">タスク一覧</h1>
-    <input type="button" class="btn btn-primary d-inline ml-5 mb-3 pl-4 pr-4" value="新規登録">
+  <div class="container">
+    <h1 class="text title">タスク一覧</h1>
+    <a href="" class="btn btn_create">新規登録</a>
     <div class="row">
-      <table class="table">
+      <table class="table_task">
         @foreach($tasks as $task)
-        <tr>
-          <td>
-            <button type="button" class="btn btn-outline-secondary">完了</button>
+        <tr class="tr">
+          <td class="td_btn">
+            <a href="" class="btn btn_done">完了</a>
           </td>
           <td>
-            <p class="lead text-secondary">{{$task->title}}</p>
+            <p class="text p_tasktitle">{{$task->title}}</p>
           </td>
-          <td>
-            <button type="button" class="btn btn-outline-secondary">編集</button>
+          <td class="td_btn">
+            <a href="" class="btn btn_edit">編集</a>
           </td>
-          <td>
-            <button type="button"  class="btn btn-outline-danger">削除</button>
+          <td class="td_btn">
+            <a href="" class="btn btn_delete">削除</a>
           </td>
         </tr>
         @endforeach
       </table>
     </div>
   </div>
-  <script src="{{ asset('js/app.js') }}"></script>
 </body>
-</html>
